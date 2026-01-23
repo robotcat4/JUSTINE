6bce62d9736281301e73851a9869024085c6952d
robotcat4 <skykennedy4@gmail.com>
2026-01-23 13:51:20 +1100

    Added dotenv integration for .env file to handle API key. Added API key loading as TBA_API_KEY and load verfication test.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask, render_template, request, redirect
 import sqlite3
+import requests
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -25,6 +27,14 @@ def init_db():
     conn.close()
 
 init_db()
+# --- .ENV initialisation ---
+load_dotenv() # Load .env file
+TBA_API_KEY = os.getenv('TBA_API_KEY') # Locate key in .env file and make usable here
+if TBA_API_KEY:  # Verify key was located
+    print(f"API Key loaded: {TBA_API_KEY[:10]}...")  # Prints first 10 characters
+else:
+    print("WARNING: TBA_API_KEY not found in .env file!")
+
 
 # --- ROUTES ---
 
